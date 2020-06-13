5737f9d684ba0d11371b7c6ba2b25dd634c8bad3
André Shindi Fucuhara <asfucuhara@live.com>
2020-06-12 23:28:45 -0300

    Delete dev.ts

--- config/dev.ts
+++
@@ -1,9 +0,0 @@
-export = {
-  PORT: 5000,
-  DISCORD_TOKEN: 'NTY5OTAyNDM2NTQ2NzcyOTky.XuQ4KA.mDmmWeMNjxDngifkZEJUfabPuZ0',
-  PREFIX: '/dys',
-};
-
-//   export TOKEN=NTY5OTAyNDM2NTQ2NzcyOTky.XL3ZtA.iPA3xg2KMdkYepH4ReNGKP963lA
-//   export PREFIX=-
-//   export GOOGLE_API_KEY=AIzaSyBvAPWUe_7uLR-X89bknKEENLT1kdaMYI
